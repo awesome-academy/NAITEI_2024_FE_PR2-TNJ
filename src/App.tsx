677e272285f4fb9026c0c52b61c2d59b0cec73c9
Tinhvila <ptinh4225@gmail.com>
2024-10-28 12:43:05 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500 text-center my-5 font-bold text-3xl">
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from '../locale/en.json';
+import vi from '../locale/vi.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
+    },
+    vi: {
+      translation: vi,
+    },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
