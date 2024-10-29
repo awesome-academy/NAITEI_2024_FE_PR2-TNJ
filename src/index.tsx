4ffddf3f85b5276ad7cfb42bea54b631054568c2
Tinhvila <ptinh4225@gmail.com>
2024-10-29 16:17:55 +0700

    UI + UX + responsive form sign-in sign-up

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './css/index.css';
+import './utils/i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
